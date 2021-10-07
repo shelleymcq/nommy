805ac4881fbd022d7c22674262a809eb84e426d4
Hunter <no@no.com>
2021-10-07 15:39:44 -0400

    clean and update search and footer icon change

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import './Footer.css'
-// import { useLocation, useHistory } from 'react-router-dom';
+
 
 const Footer = () => {
-  // const location = useLocation();
-  // const history = useHistory();
+
   return (
-    <footer className="footer w-100 mt-auto p-2">
-      <div className="container text-center mb-2">
-        {/* {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => history.goBack()}
-          >
-            &larr; Go Back
-          </button>
-        )} */}
+    <footer className="footer">
+      <div className="container">
         <h4>
           Made with
           {' '}
-          
-
-          <span>&#127863;
+          <span>&#129347;
           </span>{' '}
           by the Nommy Team.
         </h4>
