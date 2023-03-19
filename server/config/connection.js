9887193a62b258db20bb050f39b9de62d817a2b5
wevans <paul@wevans.io>
2023-03-19 12:28:53 -0400

    fix issues from reveted PR

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 // BRING IN MONGOOSE MODULE
 const mongoose = require('mongoose');
-const mongoose_user = process.env.MONGODB_USER
-const mongoose_pw = process.env.MONGODB_PASSWORD
-const mongoose_host = process.env.MONGODB_HOST
-const mongoose_port = process.env.MONGODB_PORT
+const mongoose_user = process.env.MONGODB_USER || 'root'
+const mongoose_pw = process.env.MONGODB_PASSWORD  || 'test'
+const mongoose_host = process.env.MONGODB_HOST || 'localhost'
+const mongoose_port = process.env.MONGODB_PORT || '27017'
 
 const mongoose_uri = process.env.MONGODB_URI || `mongodb://${mongoose_user}:${mongoose_pw}@${mongoose_host}:${mongoose_port}/`
 
