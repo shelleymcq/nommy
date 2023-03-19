adf8d62e4b8bac49af91334cbe4ededf9f715c04
wevans <paul@wevans.io>
2023-03-19 12:17:11 -0400

    Clean up

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,15 @@
 // BRING IN MONGOOSE MODULE
 const mongoose = require('mongoose');
+const mongoose_user = process.env.MONGODB_USER
+const mongoose_pw = process.env.MONGODB_PASSWORD
+const mongoose_host = process.env.MONGODB_HOST
+const mongoose_port = process.env.MONGODB_PORT
+
+const mongoose_uri = process.env.MONGODB_URI || `mongodb://${mongoose_user}:${mongoose_pw}@${mongoose_host}:${mongoose_port}/`
 
 // CONNECT TO MONGOOSE DATABASE
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/users',
+  mongoose_uri,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
